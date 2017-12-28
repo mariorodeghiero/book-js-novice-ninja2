a9febef538e3577d8d6a1b5bc3e6582e435cf95e
Mário Antônio do Amaral Rodeghiero <mario.rodeghiero@gmail.com>
2017-12-28 12:05:04 -0200

    started chapter 11 more  about functions

--- chapter-10/numberCruncher.test.js
+++ chapter-10/numberCruncher.test.js
@@ -1,10 +1,9 @@
 'use strict';
 function factorsOf(n) {
     const factors = [];
-    for (let i = 1; i < n; i++) {
+    for (let i = 1; i <= n; i++) { // change on this line
         if (n / i === Math.floor(n / i)) {
             factors.push(i);
         }
-    }
-    return factors;
+    } return factors;
 }
