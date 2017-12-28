bb78d5d0b4867ce0aa56e5a3f8674d8035899a44
Mário Antônio do Amaral Rodeghiero <mario.rodeghiero@gmail.com>
2017-12-28 11:49:04 -0200

    started chapter 10 with TDD

---
+++ chapter10/numberCruncher.test.js
@@ -0,0 +1,10 @@
+'use strict';
+function factorsOf(n) {
+    const factors = [];
+    for (let i = 1; i < n; i++) {
+        if (n / i === Math.floor(n / i)) {
+            factors.push(i);
+        }
+    }
+    return factors;
+}
